03de96776559746046b8a8ab7790273c015e2c58
Abel <abeljohnjoseph@gmail.com>
2020-02-09 11:59:33 -0500

    Create class with attributes

--- plotCDE.py
+++ plotCDE.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+class class_:
+	def __init__(self, n, mean, covariance):
+		self.covariance = covariance
+		self.mean = mean
+		self.n = n
+		self.cluster = 0
