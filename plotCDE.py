933279d51f30206e6e797610162493679fad964d
Abel <abeljohnjoseph@gmail.com>
2020-02-09 12:22:41 -0500

    Created objects and clusters for classes A & B

--- plotCDE.py
+++ plotCDE.py
@@ -13,10 +13,16 @@ class class_:
 		return np.random.multivariate_normal(mean, std_dev, size=size)
 
 
-c = class_(n=100, mean=np.array([5, 10]), covariance=np.array([[8, 4], [4, 40]]))
+a = class_(n=200, mean=[5, 10], covariance=[[8, 0], [0, 4]])
+b = class_(n=200, mean=[10, 15], covariance=[[8, 0], [0, 4]])
+c = class_(n=100, mean=[5, 10], covariance=[[8, 4], [4, 40]])
 d = class_(n=200, mean=[15, 10], covariance=[[8, 0], [0, 8]])
 e = class_(n=150, mean=[10, 5], covariance=[[10, -5], [-5, 20]])
 
+a.cluster = class_.create_normal_distribution(a.n, a.mean, a.covariance)
+b.cluster = class_.create_normal_distribution(b.n, b.mean, b.covariance)
 c.cluster = class_.create_normal_distribution(c.n, c.mean, c.covariance)
 d.cluster = class_.create_normal_distribution(d.n, d.mean, d.covariance)
 e.cluster = class_.create_normal_distribution(e.n, e.mean, e.covariance)
+
+plt.show()
