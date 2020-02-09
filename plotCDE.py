75c623989f2909a0bd19d96e9cfeaf37614ba715
Abel <abeljohnjoseph@gmail.com>
2020-02-09 12:01:57 -0500

    Create clusters

--- plotCDE.py
+++ plotCDE.py
@@ -12,6 +12,11 @@ class class_:
 	def create_normal_distribution(size, mean, std_dev):
 		return np.random.multivariate_normal(mean, std_dev, size=size)
 
+
 c = class_(n=100, mean=np.array([5, 10]), covariance=np.array([[8, 4], [4, 40]]))
 d = class_(n=200, mean=[15, 10], covariance=[[8, 0], [0, 8]])
 e = class_(n=150, mean=[10, 5], covariance=[[10, -5], [-5, 20]])
+
+c.cluster = class_.create_normal_distribution(c.n, c.mean, c.covariance)
+d.cluster = class_.create_normal_distribution(d.n, d.mean, d.covariance)
+e.cluster = class_.create_normal_distribution(e.n, e.mean, e.covariance)
