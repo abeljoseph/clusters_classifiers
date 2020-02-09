997e97bc1bffcfc390df568a76d35fdc0ff298a8
Abel <abeljohnjoseph@gmail.com>
2020-02-09 13:56:28 -0500

    Finish plotting and create function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from math import pi
+from math import pi, sqrt
 
 
 class class_:
@@ -9,13 +9,35 @@ class class_:
 		self.covariance = covariance
 		self.mean = mean
 		self.n = n
-		self.cluster = 0
+		self.cluster = []
 		self.eigenvals = []
 		self.eigenvecs = []
 
 	@staticmethod
 	def create_normal_distribution(size, mean, std_dev):
 		return np.random.multivariate_normal(mean, std_dev, size=size)
+
+	def plot(self):
+		max_index = np.where(self.eigenvals == max(self.eigenvals))[0][0]
+		min_index = np.where(self.eigenvals == min(self.eigenvals))[0][0]
+		largest_eigval = self.eigenvals[max_index]
+		smallest_eigval = self.eigenvals[min_index]
+		largest_eigvec = self.eigenvecs[:, max_index]
+
+		theta = np.arctan2(*largest_eigvec[::-1])
+		theta_grid = np.linspace(0, 2 * pi)
+
+		dim_a = sqrt(largest_eigval)
+		dim_b = sqrt(smallest_eigval)
+
+		axes_x = dim_a * np.cos(theta_grid)
+		axes_y = dim_b * np.sin(theta_grid)
+
+		rtn = [[np.cos(theta), np.sin(theta)], [-1 * np.sin(theta), np.cos(theta)]]
+
+		ellipse = np.matmul(np.array([axes_x, axes_y]).T, rtn)
+		plt.plot([x[0] + self.mean[0] for x in ellipse], [x[1] + self.mean[1] for x in ellipse])
+		plt.scatter([x[0] for x in self.cluster], [x[1] for x in self.cluster])
 
 
 # Instantiate classes
@@ -35,20 +57,9 @@ for cla in class_list:
 for cla in class_list:
 	cla.eigenvals, cla.eigenvecs = np.linalg.eig(cla.covariance)
 
-max_index = np.where(a.eigenvals == max(a.eigenvals))[0][0]
-min_index = np.where(a.eigenvals == min(a.eigenvals))[0][0]
-largest_eigval = a.eigenvals[max_index]
-smallest_eigval = a.eigenvals[min_index]
-largest_eigvec = a.eigenvecs[:, max_index]
-smallest_eigvec = a.eigenvecs[:, min_index]
-
-theta = np.arctan(largest_eigvec)
-print(len(theta))
-print(theta)
-
 # Create scatters
-# plt.scatter([x[0] for x in c.cluster], [x[1] for x in c.cluster])
-# plt.scatter([x[0] for x in d.cluster], [x[1] for x in d.cluster])
-# plt.scatter([x[0] for x in e.cluster], [x[1] for x in e.cluster])
+a.plot()
+b.plot()
+e.plot()
 
-# plt.show()
+plt.show()
